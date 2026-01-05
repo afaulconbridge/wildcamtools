72bf3294d0baaea718a3675965ea73e1e16589c2
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-01-05 21:02:17 +0000

    Add state testing

---
+++ tests/lib/test_states.py
@@ -0,0 +1,30 @@
+from wildcamtools.lib.motion import MogMotion
+from wildcamtools.lib.states import Watcher, WatcherStateEnum, WatcherTransitionMetrics
+from wildcamtools.lib.vidio import FrameSourceFFMPEG
+
+
+def test_states(rtsp_server: str):
+    watcher = Watcher(
+        motion=MogMotion(history=10),
+        transition_metrics=WatcherTransitionMetrics(
+            preparing_duration=10,
+            green_to_amber_motion_min=0.01,
+            amber_to_green_proportion_max=0.0075,
+            amber_to_red_duration=1,
+            red_to_red_amber_proportion_max=0.0075,
+            red_amber_to_red_proportion_min=0.01,
+            red_amber_to_green_duration=1,
+        ),
+    )
+    visited_states = {watcher.state}
+    for frame in FrameSourceFFMPEG(rtsp_server, 3840, 2160):
+        if frame.frame_no > 150:
+            break
+        watcher.handle(frame)
+        visited_states.add(watcher.state)
+
+    assert WatcherStateEnum.PREPARING in visited_states
+    assert WatcherStateEnum.GREEN in visited_states
+    assert WatcherStateEnum.AMBER in visited_states
+    assert WatcherStateEnum.RED in visited_states
+    assert WatcherStateEnum.RED_AMBER in visited_states

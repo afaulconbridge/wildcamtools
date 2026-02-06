5ec07918a83985292c45e210385081aef4da599c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-02-06 17:49:38 +0000

    remove cli states

--- src/wildcamtools/cli/__init__.py
+++ src/wildcamtools/cli/__init__.py
@@ -12,7 +12,6 @@ from wildcamtools.cli.perftest import app as perftest_app
 from wildcamtools.cli.rescale import app as rescale_app
 from wildcamtools.cli.rtsp import app as rtsp_app
 from wildcamtools.cli.segment import app as segment_app
-from wildcamtools.cli.states import app as states_app
 from wildcamtools.cli.watch import app as watch_app
 
 app = typer.Typer()
@@ -20,7 +19,6 @@ app.add_typer(rescale_app)
 app.add_typer(motion_mog2_app)
 app.add_typer(perftest_app)
 app.add_typer(rtsp_app)
-app.add_typer(states_app)
 app.add_typer(segment_app)
 app.add_typer(watch_app)
 

9a7bd033b2009de41ceaa92067a90368a8219204
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-04-15 19:33:30 +0100

    Add SSIM frame filter

--- src/wildcamtools/lib/frames.py
+++ src/wildcamtools/lib/frames.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import cv2
+from skimage.metrics import structural_similarity
 
 from wildcamtools.lib.stats import VideoStats
 
@@ -11,6 +12,7 @@ class FrameImageWriter(FrameHandler):
     def __init__(self, output_dir: Path):
         self.output_dir = output_dir
         self.output_dir.mkdir(parents=True, exist_ok=True)
+        super().__init__()
 
     def handle(self, frame: Frame) -> Frame | None:
         filename = self.output_dir / f"frame_{frame.frame_no:05d}.jpg"
@@ -42,6 +44,7 @@ class Rescaler(FrameHandler):
         self.fps = stats.fps if fps is None else fps
         self.source_frametime = stats.frame_duration
         self.target_frametime = 1000.0 / self.fps
+        super().__init__()
 
     def handle(self, frame: Frame) -> Frame | None:
         self.now += self.source_frametime
@@ -53,3 +56,32 @@ class Rescaler(FrameHandler):
         else:
             # skip this frame
             return None
+
+
+class FilterSSIM(FrameHandler):
+    """Structural Similarity Image Metric based frame filter"""
+
+    similarity_minimum: float
+    frame_previous_interesting: Frame | None = None
+
+    def __init__(self, similarity_minimum: float = 0.9):
+        self.similarity_minimum = similarity_minimum
+        super().__init__()
+
+    def handle(self, frame: Frame) -> Frame | None:
+        # no previous frame of interest, so this frame is interesting by default
+        if self.frame_previous_interesting is None:
+            self.frame_previous_interesting = frame
+            return frame
+
+        # this calculation is pretty slow
+        similarity = float(
+            structural_similarity(self.frame_previous_interesting.raw, frame.raw, data_range=255, channel_axis=2)
+        )
+
+        if similarity > self.similarity_minimum:
+            # frame is too similar to the previous interesting frame, skip it
+            return None
+        else:
+            self.frame_previous_interesting = frame
+            return frame

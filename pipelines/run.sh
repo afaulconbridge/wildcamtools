0fea92b2c49ee5ced8e3b6d3762476de4646c143
Adam Faulconbridge <afaulconbridge@googlemail.com>
2026-06-04 10:06:13 +0100

    Add AI cropper

--- pipelines/run.sh
+++ pipelines/run.sh
@@ -11,4 +11,6 @@ uv run wildcamtools ai run-evaluate pipelines/pipeline_config_verify_high.json p
 uv run wildcamtools ai run-evaluate pipelines/pipeline_config_verify_med.json pipelines/comparison_config.json wildlife/2023/labels.jsonl --output pipelines/output/verify_med.json
 uv run wildcamtools ai run-evaluate pipelines/pipeline_config_motion.json pipelines/comparison_config.json wildlife/2023/labels.jsonl --output pipelines/output/motion.json
 uv run wildcamtools ai run-evaluate pipelines/pipeline_config_ssim.json pipelines/comparison_config.json wildlife/2023/labels.jsonl --output pipelines/output/ssim.json
+uv run wildcamtools ai run-evaluate pipelines/pipeline_config.json pipelines/comparison_config_crop.json wildlife/2023/labels.jsonl --output pipelines/output/crop.json
 uv run wildcamtools ai run-evaluate pipelines/pipeline_config.json pipelines/comparison_config.json wildlife/2023/labels.jsonl --output pipelines/output/output.json
+uv run wildcamtools ai run-evaluate pipelines/pipeline_config.json pipelines/comparison_config_5x1280x720.json wildlife/2023/labels.jsonl --output pipelines/output/5x1280x720.json
